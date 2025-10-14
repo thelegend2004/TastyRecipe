0300e010ac4733f133d535776906238dee5afb92
thelegend2004 <oleh.liubichenko@gmail.com>
2025-10-15 01:51:07 +0300

    feature: add drawer navigator

--- App.js
+++ App.js
@@ -3,16 +3,55 @@ import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsScreen from "./screens/MealsScreen";
-import MealDetailsScreen from "./components/MealDetailsScreen";
-import { Button, Text } from "react-native";
+import MealDetailsScreen from "./screens/MealDetailsScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const screenStyle = {
   headerStyle: { backgroundColor: "#837c5aff" },
   headerTintColor: "white",
   contentStyle: { backgroundColor: "#827f6eff" },
 };
+
+const drawerStyle = {
+  headerStyle: { backgroundColor: "#837c5aff" },
+  headerTintColor: "white",
+  sceneStyle: { backgroundColor: "#827f6eff" },
+  drawerContentStyle: { backgroundColor: "#837c5aff" },
+  drawerInactiveTintColor: "white",
+  drawerActiveTintColor: "#ede9d3ff",
+  drawerActiveBackgroundColor: "#52470aff",
+};
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator screenOptions={drawerStyle}>
+      <Drawer.Screen
+        name="MealsCategories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="fast-food" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -22,13 +61,20 @@ export default function App() {
         <Stack.Navigator screenOptions={screenStyle}>
           <Stack.Screen
             name="Categories"
-            component={CategoriesScreen}
+            component={DrawerNavigator}
             options={{
               title: "Meal Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="Meals" component={MealsScreen} />
-          <Stack.Screen name="MealDetail" component={MealDetailsScreen} />
+          <Stack.Screen
+            name="MealDetail"
+            component={MealDetailsScreen}
+            options={{
+              title: "About the meal",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,3 +1,77 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetiail/Subtitle";
+import List from "../components/MealDetiail/List";
+import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
+
 export default function MealDetailsScreen() {
-  
+  const route = useRoute();
+  const navigation = useNavigation();
+  const { mealId } = route.params;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  function hanldeHeaderButton() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton color="white" icon="star" onPress={hanldeHeaderButton} />
+        );
+      },
+    });
+  }, [navigation, hanldeHeaderButton]);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 36,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
